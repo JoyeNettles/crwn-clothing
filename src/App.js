da92e42761d7ae2b89a7123154e8efeedbed133a
Joye Nettles <joyejnettles@gmail.com>
2020-02-19 23:24:50 -0500

    Add CustomButton and FormInput components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
+
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop-page/shop-page.component';
-import { Route, Switch } from 'react-router-dom';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -12,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signIn' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
